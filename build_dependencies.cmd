3a1aaed01ffebcba7d3336597e573c8c92c4352d
Paul E. Jones <paulej@packetizer.com>
2025-05-16 18:57:55 -0400

    Build optimizations for smaller binaries

--- build_dependencies.cmd
+++ build_dependencies.cmd
@@ -8,18 +8,13 @@
 @rem
 
 @rem Configure CMake
-cmake.exe -S . -B out/build -G Ninja ^
-    -DCMAKE_C_COMPILER:STRING=cl.exe ^
-    -DCMAKE_CXX_COMPILER:STRING=cl.exe ^
-    -DCMAKE_BUILD_TYPE:STRING=Release ^
-    -DCMAKE_INSTALL_PREFIX:PATH=out/install/x64-release ^
-    -DCMAKE_MAKE_PROGRAM=ninja.exe || ^
-        (echo cmake configuration failed && exit /b 1)
+cmake.exe -S . --preset x64-Release || ^
+    (echo cmake configuration failed && exit /b 1)
 
 @rem Build the source code
-cmake --build out/build --parallel || (echo cmake build failed && exit /b 1)
+cmake --build out/build/x64-Release --parallel || (echo cmake build failed && exit /b 1)
 
 @rem Install the dependencies locally
-cmake --install out/build || (echo cmake install failed && exit /b 1)
+cmake --install out/build/x64-Release || (echo cmake install failed && exit /b 1)
 
 echo Build and installation successful
