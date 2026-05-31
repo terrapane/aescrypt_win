d3f15fad80cfd05c45345ed08c42d81bb338a0ca
Paul E. Jones <paulej@packetizer.com>
2026-05-31 10:31:05 -0400

    Increased KDF iterations to 600,000

--- aescrypt/globals.h
+++ aescrypt/globals.h
@@ -20,7 +20,7 @@
 #include <cstdint>
 
 // Number of KDF iterations to perform when deriving key from password
-constexpr std::uint32_t KDF_Iterations = 300'000;
+constexpr std::uint32_t KDF_Iterations = 600'000;
 
 // Size in octets of buffer for file I/O
 constexpr std::size_t Buffered_IO_Size = 131'072;
